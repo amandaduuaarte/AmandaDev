c77a47288ea3719ff0a8bed2d8068b87df955d54
Amanda Duarte <amandaduuaarte@gmail.com>
2023-11-20 14:17:32 -0300

    feat: Create language component.

---
+++ src/components/Language/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "./styles";
+
+export function Language() {
+    return (
+        <Container>
+            <p>oi</p>
+        </Container>
+    );
+}
